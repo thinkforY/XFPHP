6bf3b72bc4e21fb6124b041c8f5a82bc4a8670bb
thinkforY <2321149783@qq.com>
2018-01-30 18:05:01 +0800

    登录控制器方法

--- app/admin/controller/Login.php
+++ app/admin/controller/Login.php
@@ -10,6 +10,17 @@ class Login extends Controller
 {
 	private $cache_model,$system;
 	public function index(){
+		if (request()->isPost()) {
+			$admin = new Admin();
+			$data = input("post.");
+			$num = $admin->login($data);
+			if ($num == 1) {
+				return json(['code'=>1,'msg'=>'登录成功','url'=>url('index/index')]);
+			}else{
+				return json(['code'=>0,'msg'=>'用户名或者密码错误，重新输入!']);
+			}
+		}
+		$this->cache_model = array("");
 		return $this->fetch("login");
 	}
 	//退出登录
